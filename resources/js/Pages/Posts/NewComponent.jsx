c59a615966f05d46c9f62afdd59455969f356e5b
Rost445 <rostislavgritsyk@gmail.com>
2025-11-07 00:25:46 +0200

    Use State

--- resources/js/Pages/Posts/NewComponent.jsx
+++ resources/js/Pages/Posts/NewComponent.jsx
@@ -1,12 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-export default class NewComponent extends Component{
-    
-    render(){
-         return(
-        <div className="">
-            <h1>New Component Class</h1>
-        </div>
-    )
+export default class NewComponent extends Component {
+    state = {
+        name: 'Rost',
+        age: 25,
+        phone: '+380660198521',
+        skills: ['react','vue','laravel']
+    }
+    render() {
+        return (
+            <div >
+               {/* <h1 style={{ fontSize: '3rem' }}>New Component Class</h1> */}
+               <h1 className="text-3xl font-bold">New Component Class</h1>
+
+
+                <ul>
+                    <li>name: {this.state.name}</li>
+                    <li>age: {this.state.age}</li>
+                    <li>phone: {this.state.phone}</li>
+                    <li>skills: {this.state.skills.join(', ')}</li>
+                </ul>
+            </div>
+        );
     }
 }
